d6f95b634927b8377ae94f53d62a2abd0c629a7e
Fernando Galvan <n.brighter00@gmail.com>
2022-05-09 13:14:31 -0500

    fixing click event and css

--- src/App.js
+++ src/App.js
@@ -7,29 +7,30 @@ import "./styles.css"
 export default function App() {
   const [bigImage, setBigImage] = useState(imagesArr[0].img)
   
-  const handleClick = (image)=> {
+  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
+  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  
+  const handleClick = (imageURL)=> {
     setBigImage({
-      ...bigImage,
-      img: image
+      
     })
   }
 
-  // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
-  // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
   
   // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
   // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
   const images = imagesArr.map((ele, index) =>{
-    return <img src={ele.img} alt={ele.alt} city={ele.city} key={index} onClick={handleClick} />
+    return <img className='thumb' src={ele.img} alt={ele.alt} city={ele.city} key={index} onClick={handleClick} />
   })
   return (
     <div className="App">
       <h1>Cities Of The World</h1>
       <div id="wrapper">
         <div id="thumbnails">
-          {images}
+        {images}
         </div>
-        {<img src={bigImage.img} id="bigimage" alt="bigImage"/>}
+        {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
+        {/* <img src="" id="bigimage" alt='bigImage'/> */}
       </div>
     </div>
   );
