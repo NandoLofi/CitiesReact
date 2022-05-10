e8077c0ed52588d83050818e4cb43d7cb734a6bc
Fernando Galvan <n.brighter00@gmail.com>
2022-05-10 00:08:22 -0500

    final app

--- src/App.js
+++ src/App.js
@@ -5,22 +5,19 @@ import {useState} from 'react';
 import "./styles.css"
 
 export default function App() {
-  const [bigImage, setBigImage] = useState(imagesArr[0].img)
+  const [bigImage, setBigImage] = useState(imagesArr[1].img)
   
   // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
   // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
   
   const handleClick = (imageURL)=> {
-    setBigImage({
-      
-    })
+      setBigImage(imageURL)  
   }
-
-  
+  console.log(setBigImage)
   // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
   // ASSIGN ALL OF THE PROPERTIES THAT IT NEEDS: src, alt, className, key INCLUDING AN onClick EVENT THAT CALLS THE HANDLE EVENT FUNCTION AND PASSES IT THE IMG URL
   const images = imagesArr.map((ele, index) =>{
-    return <img className='thumb' src={ele.img} alt={ele.alt} city={ele.city} key={index} onClick={handleClick} />
+    return <img className='thumb' src={ele.img} alt={ele.alt} city={ele.city} key={index} onClick={()=>{handleClick(ele.img)}} />
   })
   return (
     <div className="App">
@@ -30,7 +27,7 @@ export default function App() {
         {images}
         </div>
         {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
-        {/* <img src="" id="bigimage" alt='bigImage'/> */}
+        {<img src={bigImage} id="bigimage" alt='bigImage'/>}
       </div>
     </div>
   );
